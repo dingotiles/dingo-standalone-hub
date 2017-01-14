76ac314c36d71935be11bebf5cfd5d360f1a62f9
Dr Nic Williams <drnicwilliams@gmail.com>
2017-01-14 08:56:47 -0800

    pass thru ETCD_PROTOCOL

--- main.go
+++ main.go
@@ -91,6 +91,7 @@ func main() {
 			},
 			"etcd": map[string]interface{}{
 				"uri":      os.Getenv("ETCD_HOST_PORT"),
+				"protocol": os.Getenv("ETCD_PROTOCOL"),
 				"username": os.Getenv("ETCD_USERNAME"),
 				"password": os.Getenv("ETCD_PASSWORD"),
 			},
