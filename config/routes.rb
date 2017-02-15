26c8c7efd5e2b66057e58c978e6316f06ce50983
Dr Nic Williams <drnicwilliams@gmail.com>
2017-02-15 19:23:18 +1000

    add legacy POST /api

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get '/tutorial', to: 'docs#tutorial'
 
   post '/agent/api', to: 'agent#register_cluster_node'
+  # POST /api is legacy
+  post '/api', to: 'agent#register_cluster_node'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
