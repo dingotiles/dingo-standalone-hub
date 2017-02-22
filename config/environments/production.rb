dedb5c8084967618140f5c8de66e1f44e0b65db8
Dr Nic Williams <drnicwilliams@gmail.com>
2017-02-23 07:55:02 +1000

    experiment in allowing custom domain with -ws to pass to :443

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,8 @@ Rails.application.configure do
     application_uris = JSON.parse(ENV['VCAP_APPLICATION'])["application_uris"]
     if host = application_uris.find {|uri| uri =~ /cfapps.io/}
       config.action_cable.url = "wss://#{host}:4443/cable"
+    elsif host = application_uris.find {|uri| uri =~ /-ws/}
+      config.action_cable.url = "wss://#{host}:443/cable"
     else
       Rails.logger.error "Must deploy with a cfapps.io domain that supports :4443"
     end
